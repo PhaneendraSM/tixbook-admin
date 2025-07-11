a96b83b8d73d2390cec79996a835f6edf4ce3b48
Phaneendra S M <phaneendra@apoxeo.com>
2025-07-11 17:13:23 +0530

    username undefined error fix

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,13 +1,22 @@
 // src/components/layout/Header.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import profile from '../../assets/images/profile-icon.svg';
 
-const user = JSON.parse(localStorage.getItem('user'));
-console.log(user);
+// const user = JSON.parse(localStorage.getItem('user'));
+// console.log(user);
 
 const Header = () => {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    setUser(storedUser);
+  }, []);
+
+
   return (
     <Navbar bg="light" variant="light" expand="lg" className="ps-4 top-bar" >
       <Navbar.Brand as={Link} to="/dashboard">Admin Panel</Navbar.Brand>
@@ -15,7 +24,7 @@ const Header = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto d-flex align-items-center">
           {/* <Nav.Link as={Link} to="/profile"> */}
-          <p className='me-2 mb-0'>{user.username}</p>
+          <p className='me-2 mb-0'>{user ? user.username : ''}</p>
           <img src={profile} alt="profileicon" className='img-fluid'/>
           {/* </Nav.Link> */}
         </Nav>
